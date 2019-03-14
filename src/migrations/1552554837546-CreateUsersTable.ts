ebdd04b5e1bc8d30e44bff29dd9817c93c2f8e07
Oleksii Kulikov <yeexel@gmail.com>
2019-03-14 11:25:39 +0100

    fix build - 1

--- src/migrations/1552554837546-CreateUsersTable.ts
+++ src/migrations/1552554837546-CreateUsersTable.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateUsersTable1552554837546 implements MigrationInterface {
 
