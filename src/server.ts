09424418fec8dcd7199c8f80b92fd45920f87cad
Oleksii Kulikov <yeexel@gmail.com>
2019-03-18 13:45:50 +0100

    up

--- src/server.ts
+++ src/server.ts
@@ -35,17 +35,13 @@ app.use(async ctx => {
 });
 
 getConnectionOptions().then(async options => {
-  let dbOptions = {};
-
-  console.log(process.env.NODE_ENV);
+  let dbOptions = options;
 
   if (process.env.NODE_ENV === "production") {
     dbOptions = Object.assign(options, { extra: { ssl: "Amazon RDS" } });
   }
 
-  console.log(dbOptions);
-
-  await createConnection();
+  await createConnection(dbOptions);
 
   app.listen(config.port);
 });
