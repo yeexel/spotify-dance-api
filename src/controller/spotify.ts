f0ee6da219b01ebe54320d023548dc4e3e53bd29
Oleksii Kulikov <yeexel@gmail.com>
2019-03-20 10:48:07 +0100

    add lazy load for playlists

--- src/controller/spotify.ts
+++ src/controller/spotify.ts
@@ -14,8 +14,11 @@ class SpotifyController {
   }
 
   public static async playlists(ctx: BaseContext) {
+    const limit = ctx.query.limit || 20;
+    const offset = ctx.query.offset || 0;
+
     const playlistData = await request(
-      requestSpotifyApi(`users/${ctx.state.user.spotify_id}/playlists`, ctx.state.user.access_token)
+      requestSpotifyApi(`users/${ctx.state.user.spotify_id}/playlists?limit=${limit}&offset=${offset}`, ctx.state.user.access_token)
     );
 
     ctx.body = playlistData;
