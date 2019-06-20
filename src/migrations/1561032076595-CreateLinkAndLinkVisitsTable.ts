bf9b21fca94cf7ffb7e8f2986f1af75273176b66
Oleksii Kulikov <yeexel@gmail.com>
2019-06-20 17:04:51 +0200

    Fix lint

--- src/migrations/1561032076595-CreateLinkAndLinkVisitsTable.ts
+++ src/migrations/1561032076595-CreateLinkAndLinkVisitsTable.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateLinkAndLinkVisitsTable1561032076595 implements MigrationInterface {
 
